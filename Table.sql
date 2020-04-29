b6d72fb914944355afe7a41782168be53ef5143a
George <gec68@nau.edu>
2020-04-29 16:15:22 -0700

    Created table outlines

--- Table.sql
+++ Table.sql
@@ -1,4 +1,60 @@
-CREATE TABLE country{
-    CountryID int,
-    Name varchar(50)
-};
+/*
+Country
+- CountryID = Primary
+- CountryName
+*/
+
+/*
+Town
+- TownID = Primary
+- TownName
+> Located in Country
+> Located in State
+*/
+
+/*
+State
+- StateID = Primary
+- StateName
+*/
+
+/*
+MeetingSchedule
+- MeetID = Primary
+- MeetingLoc
+- Date
+*/
+
+/*
+Language
+- LangID = Primary
+- LangName
+- Category
+*/
+
+/*
+Account
+- AccID = Primary
+- YearJoined
+- Email
+- TimeOnPlatform (O)
+> Is of Account Level
+*/
+
+/*
+AccountLevel
+- LevelID = Primary
+- LevelName
+*/
+
+/*
+Person
+- PersonID = Primary
+- Email
+- SkillLevel
+> Lives in Town
+> Teaches Person
+> Has Meetings
+> Speaks Language
+> Owns Account
+*/
