6d6fe1918a2f62fa3c82bc76f55d99b52ac97c91
George <gec68@nau.edu>
2020-04-30 23:45:23 -0700

    Added example data

--- Table.sql
+++ Table.sql
@@ -80,42 +80,42 @@ CREATE TABLE person(
 -- Sample Data
 
 -- Country Data
-INSERT INTO country VALUES ('C001', "United States");
+INSERT INTO country VALUES ("C001", "United States");
 
 -- State Data
-INSERT INTO aState VALUES ('S001', "Arizona");
-INSERT INTO aState VALUES ('S002', "Utah");
-INSERT INTO aState VALUES ('S003', "Nevada");
+INSERT INTO aState VALUES ("S001", "Arizona");
+INSERT INTO aState VALUES ("S002", "Utah");
+INSERT INTO aState VALUES ("S003", "Nevada");
 
 -- Town Data
-INSERT INTO town VALUES ('T001', "Phoenix", 'S001', 'C001');
-INSERT INTO town VALUES ('T002', "Flagstaff", 'S001', 'C001');
-INSERT INTO town VALUES ('T011', "Salt Lake City", 'S002', 'C001');
-INSERT INTO town VALUES ('T012', "Provo", 'S002', 'C001');
-INSERT INTO town VALUES ('T021', "Reno", 'S003', 'C001');
-INSERT INTO town VALUES ('T022', "Las Vegas", 'S003', 'C001');
+INSERT INTO town VALUES ("T001", "Phoenix", "S001", "C001");
+INSERT INTO town VALUES ("T002", "Flagstaff", "S001", "C001");
+INSERT INTO town VALUES ("T011", "Salt Lake City", "S002", "C001");
+INSERT INTO town VALUES ("T012", "Provo", "S002", "C001");
+INSERT INTO town VALUES ("T021", "Reno", "S003", "C001");
+INSERT INTO town VALUES ("T022", "Las Vegas", "S003", "C001");
 
 -- Meeting Schedule Data
-INSERT INTO meetingSchedule VALUES ('M001', "Starbucks", "03/21/2020");
-INSERT INTO meetingSchedule VALUES ('M002', "Cline Library", "06/10/2020");
-INSERT INTO meetingSchedule VALUES ('M003', "Green Park", "01/30/2020");
+INSERT INTO meetingSchedule VALUES ("M001", "Starbucks", "03/21/2020");
+INSERT INTO meetingSchedule VALUES ("M002", "Cline Library", "06/10/2020");
+INSERT INTO meetingSchedule VALUES ("M003", "Green Park", "01/30/2020");
 
 -- Language Data
-INSERT INTO aLanguage VALUES ('L001', "English", "One");
-INSERT INTO aLanguage VALUES ('L002', "Spanish", "Two");
-INSERT INTO aLanguage VALUES ('L003', "French", "Three");
+INSERT INTO aLanguage VALUES ("L001", "English", "One");
+INSERT INTO aLanguage VALUES ("L002", "Spanish", "Two");
+INSERT INTO aLanguage VALUES ("L003", "French", "Three");
 
 -- Account Level Data
-INSERT INTO accountLevel VALUES ('AL01', "Beginner");
-INSERT INTO accountLevel VALUES ('AL02', "Intermediate");
-INSERT INTO accountLevel VALUES ('AL03', "Advanced");
+INSERT INTO accountLevel VALUES ("AL01", "Beginner");
+INSERT INTO accountLevel VALUES ("AL02", "Intermediate");
+INSERT INTO accountLevel VALUES ("AL03", "Advanced");
 
 -- Account Data
-INSERT INTO account VALUES ('AC01', 2016, "JohnnyB@aol.com", NULL, "AL01");
-INSERT INTO account VALUES ('AC02', 2015, "SoarAbove1962@hotmail.com", 75, "AL02");
-INSERT INTO account VALUES ('AC03', 2017, "hahaYes@gmail.com", 100, "AL03" );
+INSERT INTO account VALUES ("AC01", 2016, "JohnnyB@aol.com", NULL, "AL01");
+INSERT INTO account VALUES ("AC02", 2015, "SoarAbove1962@hotmail.com", 75, "AL02");
+INSERT INTO account VALUES ("AC03", 2017, "hahaYes@gmail.com", 100, "AL03" );
 
 -- Person Data
-INSERT INTO person VALUES ('P001', "JohnnyB@aol.com", 1, "T001", NULL, "M001", "L001", "AC01");
-INSERT INTO person VALUES ('P002', "SoarAbovie1962@hotmail.com", 2, "T002", "P001", "M002", "L002", "AC02");
+INSERT INTO person VALUES ("P001", "JohnnyB@aol.com", 1, "T001", NULL, "M001", "L001", "AC01");
+INSERT INTO person VALUES ("P002", "SoarAbovie1962@hotmail.com", 2, "T002", "P001", "M002", "L002", "AC02");
 INSERT INTO person VALUES ("P003", "hahaYes@gmail.com", 3, "T003", NULL, "M003", "L003", "AC03");
